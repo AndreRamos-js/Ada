cec3c73d68160fd66ecc6c4375f03330c75f9ad6
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-05-31 21:55:50 -0300

    Assistente Virtual
    Adicionando funções para desligamento do computador em uma hora e em meia hora, função para dizer as horas e para fechar assistente.

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from playsound import playsound
 import speech_recognition as sr
 import pyaudio
 import os
+import sys
+from datetime import datetime 
 
 
 
-def cria_audio(audio,mensagem):
+def cria_audio(audio, mensagem):
     tts = gTTS(mensagem, lang="pt-br")
     tts.save(audio)
     playsound(audio)
     os.remove(audio)
+
+cria_audio('audios/welcome.mp3','Olá sou Ada sua assistente virtual. Em que posso ajudar?')
 
 def monitora_audio():
     recon = sr.Recognizer()
@@ -22,13 +26,27 @@ def monitora_audio():
             try:
                 mensagem = recon.recognize_google(audio, language='pt-br')
                 mensagem = mensagem.lower()
-                cria_audio('audios/teste.mp3', mensagem)
+                executa_comando(mensagem)
                 break
             except sr.UnknownValueError:
                 pass
             except sr.RequestError:
                 pass
         return mensagem
+
+def executa_comando(mensagem):
+    if 'fechar assistente' in mensagem:
+        sys.exit()
+    elif 'horas' in mensagem:
+        hora = datetime.now().strftime('%H:%M')
+        frase = f'Agora são {hora}'
+        cria_audio('audios/mensagem.mp3', frase)
+    elif 'desligar computador' and 'uma hora' in mensagem:
+        os.system('shutdown -s -t 3600')
+    elif 'desligar computador' and 'meia hora' in mensagem:
+        os.system('shutdown -s -t 1800')
+    elif 'cancelar desligamento' in mensagem:
+        os.system('shutdown -a')
 
 def main():
     while True:
